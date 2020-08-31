13abc774de3be06e9e33480239e9206ac4b5a7a5
tag-wizi <tag-wizi@yandex.ru>
2020-08-31 03:50:31 +0300

    Animation fix

--- WISH/Player.gd
+++ WISH/Player.gd
@@ -1,8 +1,8 @@
 extends KinematicBody2D
 
 const SPEED = 100*1.5
-const GRAVITY = 15
-const JUMP_POWER = -250
+const GRAVITY = 10*1.5
+const JUMP_POWER = -100*3.5
 const FLOOR = Vector2(0,-1)
 
 var velocity = Vector2()
@@ -18,7 +18,7 @@ func _physics_process(_delta):
 		velocity.x = -SPEED
 		$AnimatedSprite.flip_h = true
 		if velocity.y == 0:
-			$AnimatedSprite.play("run")
+			$AnimatedSprite.play("jump")
 	else:
 		velocity.x = 0
 		if is_on_floor():
@@ -26,14 +26,15 @@ func _physics_process(_delta):
 		
 	if is_on_floor():
 		jump_count = 0
-	elif velocity.y < 0:
+	elif velocity.y > 0:
 		$AnimatedSprite.play("fall")
-	
+	else:
+		$AnimatedSprite.play("jump")
+		
 	if Input.is_action_just_pressed("ui_up"):
 		if jump_count < 2:
 			velocity.y = JUMP_POWER
 			jump_count += 1
-			$AnimatedSprite.play("jump")
 	
 	velocity.y += GRAVITY
 	
