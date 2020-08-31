d37b567980b0f8232456bfa6916f44b75ae6690b
tag-wizi <tag-wizi@yandex.ru>
2020-08-31 14:17:49 +0300

    added throw mechanic

--- WISH/Player.gd
+++ WISH/Player.gd
@@ -5,6 +5,8 @@ const GRAVITY = 10*1.5
 const JUMP_POWER = -100*3.5
 const FLOOR = Vector2(0,-1)
 
+const ROCK = preload("res://Rock.tscn")
+
 var velocity = Vector2()
 var jump_count = 0
 
@@ -12,17 +14,31 @@ func _physics_process(_delta):
 	if Input.is_action_pressed("ui_right"):
 		velocity.x = SPEED
 		$AnimatedSprite.flip_h = false
+		if sign($Position2D.position.x) == -1:
+			$Position2D.position.x *= -1
 		if velocity.y == 0:
 			$AnimatedSprite.play("run")
 	elif Input.is_action_pressed("ui_left"):
 		velocity.x = -SPEED
 		$AnimatedSprite.flip_h = true
+		if sign($Position2D.position.x) == 1:
+			$Position2D.position.x *= -1
 		if velocity.y == 0:
-			$AnimatedSprite.play("jump")
+			$AnimatedSprite.play("run")
 	else:
 		velocity.x = 0
 		if is_on_floor():
 			$AnimatedSprite.play("idle")
+		
+	if Input.is_action_just_pressed("ui_focus_next"):
+		var rock = ROCK.instance()
+		if sign($Position2D.position.x) == 1:
+			rock.set_rock_direction(1)
+		if sign($Position2D.position.x) == -1:
+			rock.set_rock_direction(-1)
+		get_parent().add_child(rock)
+		rock.position = $Position2D.global_position
+		$AnimatedSprite.play("throw")
 		
 	if is_on_floor():
 		jump_count = 0
@@ -36,6 +52,11 @@ func _physics_process(_delta):
 			velocity.y = JUMP_POWER
 			jump_count += 1
 	
+	
+	
 	velocity.y += GRAVITY
 	
 	velocity = move_and_slide(velocity, FLOOR)
+
+
+
