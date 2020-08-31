bf179917d56b7b438d23d7438fa8c5209f67f76b
tag-wizi <tag-wizi@yandex.ru>
2020-08-31 03:17:31 +0300

    added player animation

--- WISH/Player.gd
+++ WISH/Player.gd
@@ -8,21 +8,32 @@ const FLOOR = Vector2(0,-1)
 var velocity = Vector2()
 var jump_count = 0
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	if Input.is_action_pressed("ui_right"):
 		velocity.x = SPEED
+		$AnimatedSprite.flip_h = false
+		if velocity.y == 0:
+			$AnimatedSprite.play("run")
 	elif Input.is_action_pressed("ui_left"):
 		velocity.x = -SPEED
+		$AnimatedSprite.flip_h = true
+		if velocity.y == 0:
+			$AnimatedSprite.play("run")
 	else:
 		velocity.x = 0
+		if is_on_floor():
+			$AnimatedSprite.play("idle")
 		
 	if is_on_floor():
 		jump_count = 0
+	elif velocity.y < 0:
+		$AnimatedSprite.play("fall")
 	
 	if Input.is_action_just_pressed("ui_up"):
 		if jump_count < 2:
 			velocity.y = JUMP_POWER
 			jump_count += 1
+			$AnimatedSprite.play("jump")
 	
 	velocity.y += GRAVITY
 	
